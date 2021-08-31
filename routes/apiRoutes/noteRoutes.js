5e5dc1f4a56a0842f1cf157b0aa965a32ee6a205
Lightfooted <vesselofbalance@hotmail.com>
2021-08-31 13:38:22 -0400

    make it better

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const {
+  findById,
+  createNewNote,
+  validateNotes
+} = require('../../lib/note');
+const { notes } = require('../../db/db');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNotes(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
